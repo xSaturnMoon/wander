5021756b45d1ad8e61da14104078f08d80eab593
unknown <tobias.9054@gmail.com>
2026-05-19 22:54:43 +0200

    fix: ContentView Tab initialization error on iOS 18

--- Wander/ContentView.swift
+++ Wander/ContentView.swift
@@ -8,23 +8,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            Tab(value: 0, role: nil) {
-                GlobeTab()
-            } label: {
-                Image(systemName: "globe")
-            }
+            GlobeTab()
+                .tabItem {
+                    Image(systemName: "globe")
+                }
 
-            Tab(value: 1, role: nil) {
-                MapTab()
-            } label: {
-                Image(systemName: "map")
-            }
+            MapTab()
+                .tabItem {
+                    Image(systemName: "map")
+                }
 
-            Tab(value: 2, role: nil) {
-                SettingsTab()
-            } label: {
-                Image(systemName: "gearshape")
-            }
+            SettingsTab()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                }
         }
         .tabViewStyle(.sidebarAdaptable)
     }
